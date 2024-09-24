c9bcf47a8d96b1af1886b5af9063d2cb9e7369cf
pablo-fdz <pablo.fdzalb@gmail.com>
2024-09-24 20:26:55 +0200

    Exercises 7-9 completed

--- hw2_third_questions.py
+++ hw2_third_questions.py
@@ -8,7 +8,11 @@
 # number of registered cases is not the same (thus
 # the length of the lists can differ)
 
-covid_cases = {'Spain': [4, 8, 2, 0, 1], 'France': [2, 3, 6], 'Italy': [6, 8, 1, 7]}
+covid_cases = {
+    'Spain': [4, 8, 2, 0, 1], 
+    'France': [2, 3, 6], 
+    'Italy': [6, 8, 1, 7]
+    }
 
 # 7)
 # Create a function called "total_registered_cases"
@@ -47,7 +51,9 @@ def total_registered_cases_per_country(data):
 # with the total cases for each country
 covid_totals_dictionary = total_registered_cases_per_country(covid_cases)
 print(covid_totals_dictionary)
-print(max(covid_totals_dictionary))
+
+# The code below prints the country with the maximum number of COVID cases
+print(max(covid_totals_dictionary, key = covid_totals_dictionary.get))
 
 # 9)
 # Create a function called "country_with_most_cases"
@@ -59,6 +65,23 @@ print(max(covid_totals_dictionary))
 
 def country_with_most_cases(data):
     totals_dictionary = total_registered_cases_per_country(data)
+    maximum_case_country = max(totals_dictionary, 
+                               key = totals_dictionary.get)
+    # When finding the maximum, max() applies totals_dictionary.get to each key, 
+    # retrieving the associated values (the total case numbers). It then compares 
+    # these values and finds the key corresponding to the largest value.
+    return maximum_case_country
 
+# We create another sample dictionary
+covid_cases_2 = {
+    'Spain': [4, 8, 2, 0, 1], 
+    'France': [2, 3, 6], 
+    'Italy': [6, 8, 1, 7],
+    'Germany': [9, 23, 3, 2],
+    'UK': [32, 123, 56, 4]
+    }
+
+# Example with this last function, with another dictionary (it should print 'UK')
+print(country_with_most_cases(covid_cases_2))
 
     
